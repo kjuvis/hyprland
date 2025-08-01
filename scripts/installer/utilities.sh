ecf89b2c32bb293313dff21c69e2d10341d53f16
Jan <janbuchwinkler@gmail.com>
2025-08-01 11:59:13 +0200

    test

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -8,27 +8,29 @@ echo "Installation started for utilities section"
 echo "\nStarting utilities setup..."
 
 sudo pacman -S --noconfirm waybar
-cp -r /home/$SUDO_USER/simple-hyprland/configs/waybar /home/$SUDO_USER/.config/
+cp -r $SCRIPT_DIR/simple-hyprland/configs/waybar /home/$SUDO_USER/.config/
 
 yay -S --sudoloop --noconfirm tofi
-cp -r /home/$SUDO_USER/simple-hyprland/configs/tofi /home/$SUDO_USER/.config/
+cp -r $SCRIPT_DIR/simple-hyprland/configs/tofi /home/$SUDO_USER/.config/
 
 sudo pacman -S --noconfirm cliphist
 
 yay -S --sudoloop --noconfirm swww
-mkdir -p /home/$SUDO_USER/.config/assets/backgrounds && cp -r /home/$SUDO_USER/simple-hyprland/assets/backgrounds /home/$SUDO_USER/.config/assets/
+mkdir -p /home/$SUDO_USER/.config/assets/backgrounds 
+cp -r $SCRIPT_DIR/simple-hyprland/assets/backgrounds /home/$SUDO_USER/.config/assets/
 
 yay -S --sudoloop --noconfirm hyprpicker
 
 yay -S --sudoloop --noconfirm hyprlock
-cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/
+cp -r $SCRIPT_DIR/simple-hyprland/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/
 
 yay -S --sudoloop --noconfirm wlogout
-cp -r /home/$SUDO_USER/simple-hyprland/configs/wlogout /home/$SUDO_USER/.config/ && cp -r /home/$SUDO_USER/simple-hyprland/assets/wlogout /home/$SUDO_USER/.config/assets/
+cp -r $SCRIPT_DIR/simple-hyprland/configs/wlogout /home/$SUDO_USER/.config/ 
+cp -r $SCRIPT_DIR/simple-hyprland/assets/wlogout /home/$SUDO_USER/.config/assets/
 
 yay -S --sudoloop --noconfirm grimblast
 
 yay -S --sudoloop --noconfirm hypridle
-cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/
+cp -r $SCRIPT_DIR/simple-hyprland/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/
 
 echo "------------------------------------------------------------------------"
